cc5b7b082c5b66cf9dc18a753a76c2b09ee72519
thisisstreamliner <thisisstreamliner@gmail.com>
2021-04-08 12:55:46 +0300

    Signup Cubit

---
+++ lib/screens/signup/cubit/signup_cubit.dart
@@ -0,0 +1,39 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_instagram/repositories/repositories.dart';
+import 'package:meta/meta.dart';
+
+import 'package:flutter_instagram/models/models.dart';
+
+part 'signup_state.dart';
+
+class SignupCubit extends Cubit<SignupState> {
+  final AuthRepository _authRepository;
+  SignupCubit({@required AuthRepository authRepository})
+      : _authRepository = authRepository,
+        super(SignupState.initial());
+
+  void usernameChanged(String value) {
+    emit(state.copyWith(username: value, status: SignupStatus.initial));
+  }
+
+  void emailChanged(String value) {
+    emit(state.copyWith(email: value, status: SignupStatus.initial));
+  }
+
+  void passwordChanged(String value) {
+    emit(state.copyWith(password: value, status: SignupStatus.initial));
+  }
+
+  void signUpInWithCredentials() async {
+    if (!state.isFormValid || state.status == SignupStatus.submitting) return;
+    emit(state.copyWith(status: SignupStatus.submitting));
+    try {
+      await _authRepository.signUpWithEmailAndPassword(
+          user: state.username, email: state.email, password: state.password);
+      emit(state.copyWith(status: SignupStatus.sucess));
+    } on Failure catch (e) {
+      emit(state.copyWith(status: SignupStatus.error, failure: e));
+    }
+  }
+}
