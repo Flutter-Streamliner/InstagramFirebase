4ce8f5a5ed9c309c530b5f418c969135fe6d210d
thisisstreamliner <thisisstreamliner@gmail.com>
2021-04-08 23:54:28 +0300

    bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,6 +12,7 @@ import 'screens/screens.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  EquatableConfig.stringify = kDebugMode;
   Bloc.observer = SimpleBlocObserver();
   runApp(MyApp());
 }
