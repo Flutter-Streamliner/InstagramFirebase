7509fa473d71496285aaa41c53859f46b8fa058c
Tiger <thisisstreamliner@gmail.com>
2021-04-13 10:03:02 +0300

    Add Storage Repository

---
+++ lib/repositories/storage/base_storage_repository.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import 'package:meta/meta.dart';
+
+abstract class BaseStorageRepository {
+  Future<String> uploadProfileImage({
+    @required String url,
+    @required File image,
+  });
+
+  Future<String> uploadPostImage({
+    @required File image,
+  });
+}
