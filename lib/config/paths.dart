9f17513dc25a3ed84d3d670f6924a49b8373b427
thisisstreamliner <thisisstreamliner@gmail.com>
2021-04-20 10:00:25 +0300

    Create Post Repository

--- lib/config/paths.dart
+++ lib/config/paths.dart
@@ -1,4 +1,9 @@
 class Paths {
   // Top level collections.s
   static const String users = 'users';
+  static const String posts = 'posts';
+  static const String comments = 'comments';
+
+  // Subcollections
+  static const String postComments = 'postComments';
 }
