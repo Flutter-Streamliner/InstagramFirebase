5d13f4e004d97f40b7c89879ca3a55d692b9fc70
thisisstreamliner <thisisstreamliner@gmail.com>
2021-04-23 15:30:52 +0300

    Search Cubit

--- lib/repositories/user/base_user_repository.dart
+++ lib/repositories/user/base_user_repository.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_instagram/models/models.dart';
+import 'package:meta/meta.dart';
 
 abstract class BaseUserRepository {
   Future<User> getUserWithId({String userId});
   Future<void> updateUser({User user});
+  Future<List<User>> searchUsers({@required String query});
 }
